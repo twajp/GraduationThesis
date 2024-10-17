07d8a7d718063d38cbeeea4d767241beb4a6c0c8
TWA <64235761+twajp@users.noreply.github.com>
2024-10-17 13:04:07 +0900

    パスを修正 動作未確認

--- programs_for_post-processing/commands.ps1
+++ programs_for_post-processing/commands.ps1
@@ -1,31 +1,11 @@
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231109-1 北村さん\ExData_20231109-1_北村さん.csv" "T:\20231109-1 北村さん\TSLog_20231109_1114.csv" "T:\20231109-1 北村さん\ACLog_20231109_111526,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231109-1 北村さん\ACLog_20231109_112921,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231116-1 井上くん\ExData_20231116-1_井上君.csv" "T:\20231116-1 井上くん\TSLog_20231116_1316.csv" "T:\20231116-1 井上くん\ACLog_20231116_131910,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-	python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231116-2 高畑\ExData_20231116-2_高畑.csv" "T:\20231116-2 高畑\TSLog_20231116_1443 - join.csv" "T:\20231116-2 高畑\ACLog_20231116_144436,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231116-2 高畑\20231122 高畑(再)\ACLog_20231122_112759,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231116-3 桂くん\ExData_20231116-3_桂君.csv" "T:\20231116-3 桂くん\TSLog_20231116_1615.csv" "T:\20231116-3 桂く ん\ACLog_20231116_161627,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231117 矢野さん\ExData_20231117_矢野さん.csv" "T:\20231117 矢野さん\TSLog_20231117_1502.csv" "T:\20231117 矢野 さん\ACLog_20231117_150359,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231117 矢野さん\ACLog_20231117_154347,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-	python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231120 たつき\ExData_20231120_たつき.csv" "T:\20231120 たつき\TSLog_20231120_1708.csv" "T:\20231120 たつき\ACLog_20231120_171157,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231120 たつき\ACLog_20231120_174412,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231122 ドンヨン\ExData_20231122_ドンヨン.csv" "T:\20231122 ドンヨン\TSLog_20231122_1350.csv" "T:\20231122 ドンヨン\ACLog_20231122_135230,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231122 ドンヨン\ACLog_20231122_143620,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py 
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231128-2 藤岡\ExData_20231128-2_藤岡.csv" "T:\20231128-2 藤岡\TSLog_20231128_1833.csv" "T:\20231128-2 藤岡\ACLog_20231128_183208,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231128-2 藤岡\ACLog_20231128_191111,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231129-2 大野さん\ExData_20231129_大野さん.csv" "T:\20231129-2 大野さん\TSLog_20231129_1502.csv" "T:\20231129-2 大野さん\ACLog_20231129_150257,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231129-2 大野さん\ACLog_20231129_155752,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
-
-
-	python C:\Users\Tomoki\OneDrive\プログラム\自作\Python\join_exdata_tslog.py "T:\20231206 小野岡さん\ExData_20231206_小野岡さん.csv" "T:\20231206 小野岡さん\TSLog_20231206_1613 - join.csv" "T:\20231206 小野岡さん\ACLog_20231206_161437,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "T:\20231206 小野岡さん\ACLog_20231206_164758,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231109-1 北村さん/ExData_20231109-1_北村さん.csv" "../original/data20231109-1 北村さん/TSLog_20231109_1114.csv" "../original/data20231109-1 北村さん/ACLog_20231109_111526,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231109-1 北村さん/ACLog_20231109_112921,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231116-1 井上くん/ExData_20231116-1_井上君.csv" "../original/data20231116-1 井上くん/TSLog_20231116_1316.csv" "../original/data20231116-1 井上くん/ACLog_20231116_131910,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231116-2 高畑/ExData_20231116-2_高畑.csv" "../original/data20231116-2 高畑/TSLog_20231116_1443 - join.csv" "../original/data20231116-2 高畑/ACLog_20231116_144436,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231116-2 高畑/20231122 高畑(再)/ACLog_20231122_112759,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231116-3 桂くん/ExData_20231116-3_桂君.csv" "../original/data20231116-3 桂くん/TSLog_20231116_1615.csv" "../original/data20231116-3 桂くん/ACLog_20231116_161627,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231117 矢野さん/ExData_20231117_矢野さん.csv" "../original/data20231117 矢野さん/TSLog_20231117_1502.csv" "../original/data20231117 矢野さん/ACLog_20231117_150359,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231117 矢野さん/ACLog_20231117_154347,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231120 たつき/ExData_20231120_たつき.csv" "../original/data20231120 たつき/TSLog_20231120_1708.csv" "../original/data20231120 たつき/ACLog_20231120_171157,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231120 たつき/ACLog_20231120_174412,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231122 ドンヨン/ExData_20231122_ドンヨン.csv" "../original/data20231122 ドンヨン/TSLog_20231122_1350.csv" "../original/data20231122 ドンヨン/ACLog_20231122_135230,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231122 ドンヨン/ACLog_20231122_143620,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py 
+python join_exdata_tslog.py "../post-processed/data20231128-2 藤岡/ExData_20231128-2_藤岡.csv" "../original/data20231128-2 藤岡/TSLog_20231128_1833.csv" "../original/data20231128-2 藤岡/ACLog_20231128_183208,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231128-2 藤岡/ACLog_20231128_191111,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231129-2 大野さん/ExData_20231129_大野さん.csv" "../original/data20231129-2 大野さん/TSLog_20231129_1502.csv" "../original/data20231129-2 大野さん/ACLog_20231129_150257,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231129-2 大野さん/ACLog_20231129_155752,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
+python join_exdata_tslog.py "../post-processed/data20231206 小野岡さん/ExData_20231206_小野岡さん.csv" "../original/data20231206 小野岡さん/TSLog_20231206_1613 - join.csv" "../original/data20231206 小野岡さん/ACLog_20231206_161437,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv" "../original/data20231206 小野岡さん/ACLog_20231206_164758,shuto_revival_project_beta,tatsumi_pa,0,Macky,mh_nissan_gtr_nismo_r35.csv"
