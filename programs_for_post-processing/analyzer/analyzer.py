910f74b394daf6a56ec1d28596a583f7d61a3931
TWA <64235761+twajp@users.noreply.github.com>
2024-12-13 11:36:01 +0900

    左右で分析結果を分離

--- programs_for_post-processing/analyzer/analyzer.py
+++ programs_for_post-processing/analyzer/analyzer.py
@@ -12,51 +12,133 @@ with open(import_path) as f:
 
 
 sum = {
-    '1': [0, 0, 0, 0],
-    '2': [0, 0, 0, 0],
-    '3': [0, 0, 0, 0],
-    '5': [0, 0, 0, 0],
+    '1': {'both': [0, 0, 0, 0],
+          'right': [0, 0, 0, 0],
+          'left': [0, 0, 0, 0]},
+    '2': {'both': [0, 0, 0, 0],
+          'right': [0, 0, 0, 0],
+          'left': [0, 0, 0, 0]},
+    '3': {'both': [0, 0, 0, 0],
+          'right': [0, 0, 0, 0],
+          'left': [0, 0, 0, 0]},
+    '5': {'both': [0, 0, 0, 0],
+          'right': [0, 0, 0, 0],
+          'left': [0, 0, 0, 0]},
 }
 
 for i in range(0, len(imported_data)):
     if imported_data[i][7] == '1':
-        sum['1'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
-        sum['1'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
-        sum['1'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
-        sum['1'][3] += 1  # データ数
+        sum['1']['both'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+        sum['1']['both'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+        sum['1']['both'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+        sum['1']['both'][3] += 1  # データ数
+        if imported_data[i][10][0] == 'r':
+            sum['1']['right'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['1']['right'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['1']['right'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['1']['right'][3] += 1  # データ数
+        elif imported_data[i][10][0] == 'l':
+            sum['1']['left'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['1']['left'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['1']['left'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['1']['left'][3] += 1  # データ数
     elif imported_data[i][7] == '2':
-        sum['2'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
-        sum['2'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
-        sum['2'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
-        sum['2'][3] += 1  # データ数
+        sum['2']['both'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+        sum['2']['both'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+        sum['2']['both'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+        sum['2']['both'][3] += 1  # データ数
+        if imported_data[i][10][0] == 'r':
+            sum['2']['right'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['2']['right'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['2']['right'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['2']['right'][3] += 1  # データ数
+        elif imported_data[i][10][0] == 'l':
+            sum['2']['left'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['2']['left'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['2']['left'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['2']['left'][3] += 1  # データ数
     elif imported_data[i][7] == '3':
-        sum['3'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
-        sum['3'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
-        sum['3'][2] += (2/3)*3 - float(imported_data[i][4]) - float(imported_data[i][5])  # 車線変更終了からウインカーが「消える」まで
-        sum['3'][3] += 1  # データ数
+        sum['3']['both'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+        sum['3']['both'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+        sum['3']['both'][2] += (2/3)*3 - float(imported_data[i][4]) - float(imported_data[i][5])  # 車線変更終了からウインカーが「消える」まで
+        sum['3']['both'][3] += 1  # データ数
+        if imported_data[i][10][0] == 'r':
+            sum['3']['right'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['3']['right'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['3']['right'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['3']['right'][3] += 1  # データ数
+        elif imported_data[i][10][0] == 'l':
+            sum['3']['left'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['3']['left'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['3']['left'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['3']['left'][3] += 1  # データ数
     elif imported_data[i][7] == '5':
-        sum['5'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
-        sum['5'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
-        sum['5'][2] += (2/3)*5 - float(imported_data[i][4]) - float(imported_data[i][5])  # 車線変更終了からウインカーが「消える」まで
-        sum['5'][3] += 1  # データ数
+        sum['5']['both'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+        sum['5']['both'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+        sum['5']['both'][2] += (2/3)*5 - float(imported_data[i][4]) - float(imported_data[i][5])  # 車線変更終了からウインカーが「消える」まで
+        sum['5']['both'][3] += 1  # データ数
+        if imported_data[i][10][0] == 'r':
+            sum['5']['right'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['5']['right'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['5']['right'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['5']['right'][3] += 1  # データ数
+        elif imported_data[i][10][0] == 'l':
+            sum['5']['left'][0] += float(imported_data[i][4])  # ウインカーを付けてから車線変更するまで
+            sum['5']['left'][1] += float(imported_data[i][5])  # 車線変更開始～終了まで
+            sum['5']['left'][2] += float(imported_data[i][6])  # 車線変更終了からウインカーを消すまで
+            sum['5']['left'][3] += 1  # データ数
 
 average = {
-    '1': [sum['1'][0]/sum['1'][3], sum['1'][1]/sum['1'][3], sum['1'][2]/sum['1'][3]],
-    '2': [sum['2'][0]/sum['2'][3], sum['2'][1]/sum['2'][3], sum['2'][2]/sum['2'][3]],
-    '3': [sum['3'][0]/sum['3'][3], sum['3'][1]/sum['3'][3], sum['3'][2]/sum['3'][3]],
-    '5': [sum['5'][0]/sum['5'][3], sum['5'][1]/sum['5'][3], sum['5'][2]/sum['5'][3]],
+    '1': {'both': [sum['1']['both'][0]/sum['1']['both'][3], sum['1']['both'][1]/sum['1']['both'][3], sum['1']['both'][2]/sum['1']['both'][3]],
+          'right': [sum['1']['right'][0]/sum['1']['right'][3], sum['1']['right'][1]/sum['1']['right'][3], sum['1']['right'][2]/sum['1']['right'][3]],
+          'left': [sum['1']['left'][0]/sum['1']['left'][3], sum['1']['left'][1]/sum['1']['left'][3], sum['1']['left'][2]/sum['1']['left'][3]]},
+    '2': {'both': [sum['2']['both'][0]/sum['2']['both'][3], sum['2']['both'][1]/sum['2']['both'][3], sum['2']['both'][2]/sum['2']['both'][3]],
+          'right': [sum['2']['right'][0]/sum['2']['right'][3], sum['2']['right'][1]/sum['2']['right'][3], sum['2']['right'][2]/sum['2']['right'][3]],
+          'left': [sum['2']['left'][0]/sum['2']['left'][3], sum['2']['left'][1]/sum['2']['left'][3], sum['2']['left'][2]/sum['2']['left'][3]]},
+    '3': {'both': [sum['3']['both'][0]/sum['3']['both'][3], sum['3']['both'][1]/sum['3']['both'][3], sum['3']['both'][2]/sum['3']['both'][3]],
+          'right': [sum['3']['right'][0]/sum['3']['right'][3], sum['3']['right'][1]/sum['3']['right'][3], sum['3']['right'][2]/sum['3']['right'][3]],
+          'left': [sum['3']['left'][0]/sum['3']['left'][3], sum['3']['left'][1]/sum['3']['left'][3], sum['3']['left'][2]/sum['3']['left'][3]]},
+    '5': {'both': [sum['5']['both'][0]/sum['5']['both'][3], sum['5']['both'][1]/sum['5']['both'][3], sum['5']['both'][2]/sum['5']['both'][3]],
+          'right': [sum['5']['right'][0]/sum['5']['right'][3], sum['5']['right'][1]/sum['5']['right'][3], sum['5']['right'][2]/sum['5']['right'][3]],
+          'left': [sum['5']['left'][0]/sum['5']['left'][3], sum['5']['left'][1]/sum['5']['left'][3], sum['5']['left'][2]/sum['5']['left'][3]]},
 }
 pprint(average)
-print(average['1'])
-with open('../../data/post-processed/Analysis_1.csv', 'a', newline='') as f:
+with open('../../data/post-processed/Analysis_1_both.csv', 'a', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(average['1'])
-with open('../../data/post-processed/Analysis_2.csv', 'a', newline='') as f:
+    writer.writerow(average['1']['both'])
+with open('../../data/post-processed/Analysis_1_right.csv', 'a', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(average['2'])
-with open('../../data/post-processed/Analysis_3.csv', 'a', newline='') as f:
+    writer.writerow(average['1']['right'])
+with open('../../data/post-processed/Analysis_1_left.csv', 'a', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(average['3'])
-with open('../../data/post-processed/Analysis_5.csv', 'a', newline='') as f:
+    writer.writerow(average['1']['left'])
+
+with open('../../data/post-processed/Analysis_2_both.csv', 'a', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(average['5'])
+    writer.writerow(average['2']['both'])
+with open('../../data/post-processed/Analysis_2_right.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['2']['right'])
+with open('../../data/post-processed/Analysis_2_left.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['2']['left'])
+
+with open('../../data/post-processed/Analysis_3_both.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['3']['both'])
+with open('../../data/post-processed/Analysis_3_right.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['3']['right'])
+with open('../../data/post-processed/Analysis_3_left.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['3']['left'])
+
+with open('../../data/post-processed/Analysis_5_both.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['5']['both'])
+with open('../../data/post-processed/Analysis_5_right.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['5']['right'])
+with open('../../data/post-processed/Analysis_5_left.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(average['5']['left'])
